b63f4bc2b6802cca6ba01b7f391e0fef6aca5213
Chamodh Abeygoonawardena <it20189044@my.sliit.lk>
2025-06-17 02:33:07 +0530

    dEVELOPMENT DONE

--- Everast_Garment/everast_new/include/menubar.php
+++ Everast_Garment/everast_new/include/menubar.php
@@ -353,6 +353,12 @@ else if ($lastElement == 'rptcustomerorderholdstock.php') {
     $statuscheck = checkprivilege($menuprivilegearray, 69, 3);
     $deletecheck = checkprivilege($menuprivilegearray, 69, 4);
 }
+else if ($lastElement == 'rptcustomeroutstandingexcel.php') {
+    $addcheck    = checkprivilege($menuprivilegearray, 70, 1);
+    $editcheck   = checkprivilege($menuprivilegearray, 70, 2);
+    $statuscheck = checkprivilege($menuprivilegearray, 70, 3);
+    $deletecheck = checkprivilege($menuprivilegearray, 70, 4);
+}
 
 
 
@@ -654,14 +660,14 @@ function checkprivilege($arraymenu, $menuID, $type)
                 </nav>
             </div>
             <?php }
-                if (menucheck($menuprivilegearray, 31) == 1 | menucheck($menuprivilegearray, 32) == 1 | menucheck($menuprivilegearray, 33) == 1 | menucheck($menuprivilegearray, 34) == 1 | menucheck($menuprivilegearray, 35) == 1 | menucheck($menuprivilegearray, 36) == 1 | menucheck($menuprivilegearray, 37) == 1 | menucheck($menuprivilegearray, 38) == 1 | menucheck($menuprivilegearray, 39) == 1 | menucheck($menuprivilegearray, 48) == 1 | menucheck($menuprivilegearray, 49) == 1 | menucheck($menuprivilegearray, 57) == 1 | menucheck($menuprivilegearray, 59) == 1 | menucheck($menuprivilegearray, 61) == 1 | menucheck($menuprivilegearray, 62) == 1 | menucheck($menuprivilegearray, 63) == 1 | menucheck($menuprivilegearray, 64) == 1  | menucheck($menuprivilegearray, 69) == 1 ) { ?>
+                if (menucheck($menuprivilegearray, 31) == 1 | menucheck($menuprivilegearray, 32) == 1 | menucheck($menuprivilegearray, 33) == 1 | menucheck($menuprivilegearray, 34) == 1 | menucheck($menuprivilegearray, 35) == 1 | menucheck($menuprivilegearray, 36) == 1 | menucheck($menuprivilegearray, 37) == 1 | menucheck($menuprivilegearray, 38) == 1 | menucheck($menuprivilegearray, 39) == 1 | menucheck($menuprivilegearray, 48) == 1 | menucheck($menuprivilegearray, 49) == 1 | menucheck($menuprivilegearray, 57) == 1 | menucheck($menuprivilegearray, 59) == 1 | menucheck($menuprivilegearray, 61) == 1 | menucheck($menuprivilegearray, 62) == 1 | menucheck($menuprivilegearray, 63) == 1 | menucheck($menuprivilegearray, 64) == 1  | menucheck($menuprivilegearray, 69) == 1 | menucheck($menuprivilegearray, 70) == 1 ) { ?>
             <a class="nav-link p-0 px-3 py-2 collapsed" href="javascript:void(0);" data-toggle="collapse"
                 data-target="#collapsereport" aria-expanded="false" aria-controls="collapsereport">
                 <div class="nav-link-icon"><i data-feather="file"></i></div>
                 Reports
                 <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
             </a>
-            <div class="collapse <?php if ($lastElement == "stock.php" | $lastElement == "customeroutstanding.php" | $lastElement == "dailysale.php" | $lastElement == "dailycash.php" | $lastElement == "salereportcustomer.php" | $lastElement == "accountreport.php" | $lastElement == "bufferstockmaintainreport.php" | $lastElement == "ouritemrange.php" | $lastElement == "salesorder.php" | $lastElement == "bincard.php" | $lastElement == "overpaymentreport.php" | $lastElement == "productinvoicesreport.php" | $lastElement == "rptrepvisesales.php" | $lastElement == "rptcustomeroutstanding.php" | $lastElement == "rptsalerepcollections.php" | $lastElement == "rptinventorydetails.php" | $lastElement == "rptstockadjustmentdetails.php" | $lastElement == "rptitemviseprofitreport.php" | $lastElement == "rptinvoiceviseprofitreport.php" | $lastElement == "rptcustomerorderholdstock.php") {echo 'show';} ?>"
+            <div class="collapse <?php if ($lastElement == "stock.php" | $lastElement == "customeroutstanding.php" | $lastElement == "dailysale.php" | $lastElement == "dailycash.php" | $lastElement == "salereportcustomer.php" | $lastElement == "accountreport.php" | $lastElement == "bufferstockmaintainreport.php" | $lastElement == "ouritemrange.php" | $lastElement == "salesorder.php" | $lastElement == "bincard.php" | $lastElement == "overpaymentreport.php" | $lastElement == "productinvoicesreport.php" | $lastElement == "rptrepvisesales.php" | $lastElement == "rptcustomeroutstanding.php" | $lastElement == "rptsalerepcollections.php" | $lastElement == "rptinventorydetails.php" | $lastElement == "rptstockadjustmentdetails.php" | $lastElement == "rptitemviseprofitreport.php" | $lastElement == "rptinvoiceviseprofitreport.php" | $lastElement == "rptcustomerorderholdstock.php" | $lastElement == "rptcustomeroutstandingexcel.php") {echo 'show';} ?>"
                 id="collapsereport" data-parent="#accordionSidenav">
                 <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
                     <?php if (menucheck($menuprivilegearray, 31) == 1) { ?>
@@ -693,6 +699,9 @@ function checkprivilege($arraymenu, $menuID, $type)
                     <?php }
                                     if (menucheck($menuprivilegearray, 48) == 1) { ?>
                     <a class="nav-link p-0 px-3 py-1" href="overpaymentreport.php">Customer Overpayment</a>
+                    <?php }
+                                    if (menucheck($menuprivilegearray, 70) == 1) { ?>
+                    <a class="nav-link p-0 px-3 py-1" href="rptcustomeroutstandingexcel.php">Customer Overpayment Excel</a>
                     <?php }
                                     if (menucheck($menuprivilegearray, 49) == 1) { ?>
                     <a class="nav-link p-0 px-3 py-1" href="productinvoicesreport.php">Product Invoices</a>
